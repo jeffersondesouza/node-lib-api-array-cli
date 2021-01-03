e4d184792d7ca927859752571dedfa995d7f8bbc
Jefferson <jeffersondesouzacordel@gmail.com>
2021-01-03 07:32:13 -0300

    add async modes

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
 import fs from "fs";
 import chalk from "chalk";
 
+function handleFileText(txt) {
+  console.log(chalk.green(txt));
+}
+
+function finishFileReading(txt) {
+  console.log(chalk.yellow("Reading file finished"));
+}
+
 function errorHandler(error) {
   console.log(error);
   throw new Error(chalk.red(error.code, "Error during file importing"));
 }
 
-function readFile(filePath) {
+function readFileSync(filePath) {
   fs.readFile(filePath, "utf-8", function name(error, txt) {
     if (error) {
       errorHandler(error);
     }
-    console.log(chalk.green(txt));
+    handleFileText(txt);
+    finishFileReading();
   });
 }
 
-readFile("./files/text.md");
+function readFileAsync(filePath) {
+  fs.promises
+    .readFile(filePath, "utf-8")
+    .then(handleFileText)
+    .catch(errorHandler)
+    .finally(finishFileReading);
+}
+
+async function readFileAsyncAwait(filePath) {
+  try {
+    const txt = await fs.promises.readFile(filePath, "utf-8");
+    handleFileText(txt);
+  } catch (error) {
+    errorHandler(error);
+  } finally {
+    finishFileReading();
+  }
+}
+
+readFileSync("./files/text.md");
+readFileAsync("./files/text.md");
+readFileAsyncAwait("./files/text.md");
